e9c2131fb0b9841356f4b02f4c6fef313cb92e45
Miv2nir <miv2nir@gmail.com>
2023-04-13 23:05:19 +0300

    stylizing the entire app

--- vedsite/demo/forms.py
+++ vedsite/demo/forms.py
@@ -2,15 +2,15 @@ from django import forms
 
 
 class AuthForm(forms.Form):
-    login = forms.CharField(label='login', max_length=100)
-    password = forms.CharField(label='password', max_length=100, widget=forms.PasswordInput)
+    login = forms.CharField(label='Login', max_length=100)
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
 
 
 class RegisterForm(forms.Form):
-    email = forms.CharField(label='email', max_length=100, widget=forms.EmailInput)
-    login = forms.CharField(label='login', max_length=100)
-    password = forms.CharField(label='password', max_length=100, widget=forms.PasswordInput)
-    password_verify = forms.CharField(label='password_verify', max_length=100, widget=forms.PasswordInput)
+    email = forms.CharField(label='Email', max_length=100, widget=forms.EmailInput)
+    login = forms.CharField(label='Login', max_length=100)
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
+    password_verify = forms.CharField(label='Repeat Password', max_length=100, widget=forms.PasswordInput)
 
 
 class LogOutForm(forms.Form):
@@ -19,5 +19,5 @@ class LogOutForm(forms.Form):
 
 class CredentialsForm(forms.Form):
     ya_login = forms.CharField(label='Yandex.Contest Login', max_length=100)
-    ya_password = forms.CharField(label='Yandex.Contest Passwoed', max_length=100, widget=forms.PasswordInput)
+    ya_password = forms.CharField(label='Yandex.Contest Password', max_length=100, widget=forms.PasswordInput)
     ya_id = forms.IntegerField(label='Yandex.Contest Contest ID')
